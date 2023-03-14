afa24c05b94fb29b3e3da63dc480343e29138244
Algod <121738924+Algod001@users.noreply.github.com>
2023-03-14 19:08:47 +0545

    stack in linked list

--- stack.py
+++ stack.py
@@ -1,1 +1,45 @@
 
+class Element:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList:
+    def __init__(self, head=None):
+        self.head = head
+        
+    def append(self, new_element):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
+
+    def insert_first(self, new_element):
+        current = self.head
+        self.head = new_element
+        new_element.next = current
+
+    def delete_first(self):
+   
+        if self.head:
+            deleted_element = self.head
+            self.head = deleted_element.next
+            deleted_element.next = None
+            return deleted_element
+
+class Stack:
+    def __init__(self,top=None):
+        self.ll = LinkedList(top)
+
+    def push(self, new_element):
+
+        current = self.ll.head
+        self.ll.head = new_element
+        new_element.next = current
+
+    def pop(self):
+        return self.ll.delete_first()
+
