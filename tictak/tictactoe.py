81a14195400a7183e9f4ccb20452cb27b3df6cd2
Bishwas Aryal <aryalbishwas123@gmail.com>
2023-03-17 00:44:36 +0545

    Adding ml tic-tac-tok game

---
+++ tictak/tictactoe.py
@@ -0,0 +1,102 @@
+import math
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    countX = 0
+    countO = 0
+    for row in board:
+        for cell in row:
+            if cell == X:
+                countX += 1
+            elif cell == O:
+                countO += 1
+    if countX == countO:
+        return X
+    else:
+        return O
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    actions = set()
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                actions.add((i, j))
+    return actions
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    i, j = action
+    new_board = [row[:] for row in board]
+    new_board[i][j] = player(board)
+    return new_board
+
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] != EMPTY:
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i] != EMPTY:
+            return board[0][i]
+    if board[0][0] == board[1][1] == board[2][2] != EMPTY:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != EMPTY:
+        return board[0][2]
+    return None
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    if winner(board) is not None:
+        return True
+    for row in board:
+        for cell in row:
+            if cell == EMPTY:
+                return False
+    return True
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    if winner(board) == X:
+        return 1
+    elif winner(board) == O:
+        return -1
+    else:
+        return 0
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    raise NotImplementedError
